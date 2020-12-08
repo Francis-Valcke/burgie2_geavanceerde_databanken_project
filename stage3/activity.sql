59fe9c42c7f2f4b6fa22979a996fd53d4e557e9e
Francis <valckef@gmail.com>
2020-12-08 11:43:40 +0100

    Edit rules

--- stage3/activity.sql
+++ stage3/activity.sql
@@ -6,24 +6,108 @@ declare
 begin
 	set search_path to tourism1;
 		
+	--  First make a join table to perform queries on
+	
+	drop table if exists activity_full;
+	create temporary table activity_full as (
+		select 
+			activity.activity_id,
+		
+			activity.accessibility_info_url,
+			activity.accessibility_label,
+			accessibility_information.motoric_accessibility,
+			
+			case when (accessibility_information.motoric_accessibility is NULL) or (accessibility_information.motoric_accessibility = 0) then false else true end as motoric_accessibility_bool,
+		
+			allergy_information.allergy,
+			allergy_information.allergy_details,
+			allergy_information.food_allergy,
+			
+			case when (allergy_information.allergy is NULL) or (allergy_information.allergy = 0) then false else true end as allergy_bool,
+			case when (allergy_information.food_allergy is NULL) or (allergy_information.food_allergy = 0) then false else true end as food_allergy_bool,
+		
+			auditive_disability_information.deaf,
+			auditive_disability_information.deaf_details,
+			auditive_disability_information.hearing_impaired,
+			auditive_disability_information.hearing_impaired_details,
+		
+			case when (auditive_disability_information.deaf is NULL) or (auditive_disability_information.deaf = 0) then false else true end as deaf_bool,
+			case when (auditive_disability_information.hearing_impaired is NULL) or (auditive_disability_information.hearing_impaired = 0) then false else true end as hearing_impaired_bool,
+
+			mental_disability_information.autism,
+			mental_disability_information.autism_details,
+			mental_disability_information.mental_disability,
+			mental_disability_information.mental_disability_details,
+		
+			case when (mental_disability_information.autism is NULL) or (mental_disability_information.autism = 0) then false else true end as autism_bool,
+			case when (mental_disability_information.mental_disability is NULL) or (mental_disability_information.mental_disability = 0) then false else true end as mental_disability_bool,
+		
+			visual_disability_information.blind,
+			visual_disability_information.blind_details,
+			visual_disability_information.visually_impaired,
+			visual_disability_information.visually_impaired_details,
+		
+			case when (visual_disability_information.blind is NULL) or (visual_disability_information.blind = 0) then false else true end as blind_bool,
+			case when (visual_disability_information.visually_impaired is NULL) or (visual_disability_information.visually_impaired = 0) then false else true end as visually_impaired_bool
+		
+			
+		from activity
+		join accessibility_information on accessibility_information.activity_id = activity.activity_id
+	  	join allergy_information on allergy_information.activity_id = activity.activity_id
+ 		join auditive_disability_information on auditive_disability_information.activity_id = activity.activity_id
+ 		join mental_disability_information on mental_disability_information.activity_id = activity.activity_id
+ 		join visual_disability_information on visual_disability_information.activity_id = activity.activity_id
+	);
+			
 	-- Check accessibility URL validity
 	for record in (
-		select activity_id, accessibility_info_url
-		from activity
-		where 
-			(
-				not cast(accessibility_info_url as text) ~ '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^\s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^\s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^\s]{2,}|www.[a-zA-Z0-9]+.[^\s]{2,})'
-				or cast(accessibility_info_url as text) ~ '^(.*\s+.*)+$'
-			) 
-			and not (
-				cast(accessibility_info_url as text) like '' and accessibility_label like ''
-			)
-	)	
-	loop
+ 		select activity_id, accessibility_info_url
+ 		from activity_full
+ 		where 
+ 			(
+ 				not cast(accessibility_info_url as text) ~ '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^\s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^\s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^\s]{2,}|www.[a-zA-Z0-9]+.[^\s]{2,})'
+ 				or cast(accessibility_info_url as text) ~ '^(.*\s+.*)+$'
+ 			) 
+ 			and not (
+ 				cast(accessibility_info_url as text) like '' and accessibility_label like ''
+ 			)
+ 	)	
+ 	loop
 		raise notice 'Activity_id = %: Inconsistant accessibility_info_url = %', record.activity_id, record.accessibility_info_url;
+ 	end loop;
+	
+	for record in (
+		select *
+		from activity_full
+		where deaf_bool = true and hearing_impaired_bool = false
+	) loop
+		raise notice 'Activity_id = %: deaf = %, hearing_impared = %', record.activity_id, record.deaf_bool, record.hearing_impaired_bool;
 	end loop;
 	
+	for record in (
+		select *
+		from activity_full
+		where blind_bool = true and visually_impaired_bool = false
+	) loop
+		raise notice 'Activity_id = %: blind = %, visually_impaired = %', record.activity_id, record.blind_bool, record.visually_impaired_bool;
+	end loop;
 	
+	for record in (
+		select *
+		from activity_full
+		where autism_bool = true and mental_disability_bool = false
+	) loop
+		raise notice 'Activity_id = %: autism = %, mental_disability = %', record.activity_id, record.autism_bool, record.mental_disability_bool;
+	end loop;
+
+	for record in (
+		select *
+		from activity_full
+		where food_allergy_bool = true and allergy_bool = false
+	) loop
+		raise notice 'Activity_id = %:  food_allergy = %, allergy = %', record.activity_id, record.food_allergy_bool, record.allergy_bool;
+	end loop;
 	
+	drop table if exists activity_full;
 end;
 $$ language plpgsql;
