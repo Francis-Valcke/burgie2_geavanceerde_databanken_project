ff25d8af0e4c1c7966ac7b7dd74cdaaf61f8e3e5
Francis <valckef@gmail.com>
2020-12-10 20:36:14 +0100

    Add visitor without duplicate

--- stage5/etl_visitor_repair.sql
+++ stage5/etl_visitor_repair.sql
@@ -112,12 +112,25 @@ end;
 $$ language plpgsql;
 
 
+
+
+
+
 do $$
 declare
+
 	record record;
-	corrected_id int = null;
+	now timestamp := current_timestamp;
+	scd1_change integer := 0;
+	scd2_change integer := 0;
+	row_inserts integer := 0;
+	total_rows_before_insert integer := 0;
+	total_rows_after_insert integer := 0;
+	total_rows_after_delete integer := 0;
+	deleted_rows integer := 0;
+
+begin
 	
-begin	
 	set client_min_messages = error;
 	create table if not exists duplicates as (
 		-- Actual code for selecting the duplicates
@@ -178,29 +191,24 @@ begin
 		where owa_score > 0.015
 		order by visitor_id_1, visitor_id_2
 	);
+
 	set client_min_messages = notice;
 	set search_path to tourism1;
 
-	create temporary table if not exists translation_table (
-		old_visitor_id integer,
-		new_visitor_id integer,
-		most_recent_reservation timestamp
-	) on commit drop ;
-
-
-	for record in (
-		with duplicates_reservation_time as (	
+	-- create temporary table if not exists translation_table on commit drop as (
+	create table if not exists public.translation_table as (
+		with duplicates_reservation_time as (
 			select distinct
-			visitor_id_1,
-			visitor_id_2,
-			last_value(r1.booking_time) over (partition by visitor_id_1 order by r1.booking_time 
-					RANGE BETWEEN
-					UNBOUNDED PRECEDING AND
-					UNBOUNDED FOLLOWING) as r1_most_recent_reservation,
-			last_value(r2.booking_time) over (partition by visitor_id_2 order by r2.booking_time 
-					RANGE BETWEEN
-					UNBOUNDED PRECEDING AND
-					UNBOUNDED FOLLOWING) as r2_most_recent_reservation
+				visitor_id_1,
+				visitor_id_2,
+				last_value(r1.booking_time) over (partition by visitor_id_1 order by r1.booking_time 
+						RANGE BETWEEN
+						UNBOUNDED PRECEDING AND
+						UNBOUNDED FOLLOWING) as r1_most_recent_reservation,
+				last_value(r2.booking_time) over (partition by visitor_id_2 order by r2.booking_time 
+						RANGE BETWEEN
+						UNBOUNDED PRECEDING AND
+						UNBOUNDED FOLLOWING) as r2_most_recent_reservation
 			from public.duplicates
 			left join reservation r1 on r1.visitor_id = duplicates.visitor_id_1
 			left join reservation r2 on r2.visitor_id = duplicates.visitor_id_2
@@ -221,46 +229,199 @@ begin
 				when r1_most_recent_reservation > r2_most_recent_reservation then visitor_id_1
 				else visitor_id_2
 				end
-				as new_visitor_id,
-
-			case
-				when r1_most_recent_reservation is NULL then r2_most_recent_reservation
-				when r2_most_recent_reservation is NULL then r1_most_recent_reservation
-				when r1_most_recent_reservation > r2_most_recent_reservation then r1_most_recent_reservation
-				else r2_most_recent_reservation
-				end
-				as most_recent_reservation
+				as new_visitor_id
 		from duplicates_reservation_time
-	)
+	);
+
+
+	select count(*) from public.visitor into total_rows_before_insert;
+	
+	for record in (
+		with temp_table as (
+			select 
+				visitor.firstname,
+				visitor.email_address,
+				(cast(visitor.birthdate as text):: timestamp) as birthdate,
+				visitor.surname,
+				visitor.visitor_id,
+				address.zipcode,
+				address.housenumber,
+				address.municipality,
+				address.street_name,
+				now as validity_start
+			from visitor
+			join address on visitor.address_id=address.address_id
+		)
+		select
+			*,
+			extract(year from temp_table.birthdate) as birthdate_year,
+			extract(month from temp_table.birthdate) as birthdate_month,
+			extract(day from temp_table.birthdate) as birthdate_day,
+			extract(hour from temp_table.birthdate) as birthdate_hour,
+			extract(minute from temp_table.birthdate) as birthdate_minute,
+			extract(second from temp_table.birthdate) as birthdate_second,
+			extract(epoch from temp_table.birthdate) as birthdate_epoch
+		from temp_table
+	) 
 	loop
 		
-		if exists (
-			select *
-			from translation_table
-			where 
-				record.new_visitor_id = old_visitor_id
--- 				and record.most_recent_reservation > most_recent_reservation
-		) 
+		if record.visitor_id not in (
+			select old_visitor_id
+			from public.translation_table
+		)
 		then
-			-- There one that was found needs to be corrected
-			raise notice 'eyoo';
-			
-			
-		else
-			insert into translation_table (
-				old_visitor_id,
-				new_visitor_id,
-				most_recent_reservation
-			) values (
-				record.old_visitor_id,
-				record.new_visitor_id,
-				record.most_recent_reservation
+		
+			-- Update SCD1 fields of existing rows
+			if exists (
+				select
+					visitor_id
+				from
+					visitor
+				where
+					visitor_id = record.visitor_id
+			)
+			then
+				update 
+					public.visitor
+				set 
+					email_address = record.email_address,
+					firstname = record.firstname,
+					surname = record.surname,
+					birthdate_time_id = record.birthdate_epoch
+				where 
+					visitor_id = record.visitor_id;
+
+				scd1_change := scd1_change + 1;
+			end if;
+
+
+			-- Insert the new row
+				-- time_dimension_entry for birthdate
+			insert into public.time_dimension(
+				year,
+				month,
+				day,
+				hour,
+				minute,
+				second,
+				epoch
+			)
+			values (
+				record.birthdate_year,
+				record.birthdate_month,
+				record.birthdate_day,
+				record.birthdate_hour,
+				record.birthdate_minute,
+				record.birthdate_second,
+				record.birthdate_epoch
+			) on conflict do nothing;
+
+				-- visitor_entry
+			insert into public.visitor(
+				visitor_id,
+				firstname,
+				surname,
+				email_address,
+				address_zipcode,
+				address_housenumber,
+				address_municipality,
+				address_street_name,
+				birthdate_time_id,
+				validity_start
+			)
+			values (
+				record.visitor_id,
+				record.firstname,
+				record.surname,
+				record.email_address,
+				record.zipcode,
+				record.housenumber,
+				record.municipality,
+				record.street_name,
+				record.birthdate_epoch,
+				record.validity_start
 			);
-			
+
+			row_inserts := row_inserts + 1;
+		
+		else
+			raise notice 'skipping duplicate with visitor_id: %', record.visitor_id;
 		end if;
 		
--- 		raise notice 'corrected_id = %', record.new_visitor_id;
-	
 	end loop;
+	
+	raise notice 'scd1_updates: %', scd1_change;
+	raise notice 'row_inserts: %', row_inserts;
+	select count(*) from public.visitor into total_rows_after_insert;
+
+	-- look for changed scd2 attributes as to decide wether to keep or delete the newly added version
+	with comparison_last as (
+		select 
+			visitor_id,
+			validity_start, 
+		
+			address_zipcode, 
+			address_housenumber, 
+			address_street_name, 
+			address_municipality, 
+		
+			lag(address_zipcode) over w as last_zipcode,
+			lag(address_housenumber) over w as last_housenumber,
+			lag(address_street_name) over w as last_street_name,
+			lag(address_municipality) over w as last_municipality
+		from 
+			public.visitor
+		window w as (partition by visitor_id order by validity_start)
+	),
+	
+	-- search for duplicate rows
+	remove_versions as (
+		select 
+			visitor_id, 
+			validity_start 
+		from 
+			comparison_last
+		where
+			address_zipcode is not distinct from last_zipcode and
+			address_housenumber is not distinct from last_housenumber and
+			address_municipality is not distinct from last_municipality and
+			address_street_name is not distinct from last_street_name
+	),		
+	
+	-- delete duplicate rows
+	deleted_rows as (
+		delete from public.visitor
+	 	where (
+			visitor_id, 
+			validity_start
+		) in (select * from remove_versions) returning *
+	)
+
+	select count(*) from deleted_rows into deleted_rows; 
+  	raise notice 'Deleted rows: %', deleted_rows;
+	
+	-- update validity_end
+	update 
+		public.visitor v
+	set 
+		validity_end = newv.validity_end 
+	from (
+		select 
+			visitor_id, 
+			validity_start, 
+			lead(validity_start) over w as validity_end
+		from public.visitor
+		window w as (partition by visitor_id order by validity_start)
+		order by visitor_id, validity_start asc
+	) newv
+	where 
+		newv.visitor_id = v.visitor_id and newv.validity_start = v.validity_start;
+		
+	select count(*) from public.visitor into total_rows_after_delete;
+	
+	raise notice 'total_rows_before_insert: %', total_rows_before_insert;
+	raise notice 'total_rows_after_insert: %', total_rows_after_insert;
+	raise notice 'total_rows_after_delete: %', total_rows_after_delete;
+	
 end;
-$$ language plpgsql
+$$ language plpgsql;
